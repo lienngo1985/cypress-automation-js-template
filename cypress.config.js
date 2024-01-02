aa904fd980c313275067430b96f3442533425e26
Lien Ngo <ana.ngo.1985@gmail.com>
2024-01-02 17:34:50 +0700

    Add packages for mochawesome reports
    Use multi cypress-multi-reporters, including
     - cypress-mochawesome-reporter to generate HTML report.
     - mocha-junit-reporter to generate XML report.

--- cypress.config.js
+++ cypress.config.js
@@ -1,28 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: 'cypress-multi-reporters',
   reporterOptions: {
-    mochaFile: 'results/cypress-report-[hash].xml',
-    toConsole: true,
-    charts: true,
-    reportPageTitle: 'custom-title',
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-
-    useInlineDiffs: true,
-    embeddedScreenshots: true,
-    reportDir: 'cypress/results',
-    reportFilename: '[name].html',
-    overwrite: true,
-    html: true,
-    json: true,
+    configFile: 'reporter-config.json',
   },
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,8 +14,16 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
 import 'cypress-mochawesome-reporter/register';
+import addContext from 'mochawesome/addContext'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on("test:after:run", (test, runnable) => {  
+    if (test.state === "failed") {    
+        const screenshot =`assets/snapshots/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;    
+        addContext({ test }, screenshot);  
+    }
+});
