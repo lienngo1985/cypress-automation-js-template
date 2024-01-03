60265305c173b6042738a2b1aa4576e6bb74b453
Lien Ngo <ana.ngo.1985@gmail.com>
2024-01-03 22:20:51 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,28 +1,5 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    mochaFile: 'results/cypress-report-[hash].xml',
-    toConsole: true,
-    charts: true,
-    reportPageTitle: 'custom-title',
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-
-    useInlineDiffs: true,
-    embeddedScreenshots: true,
-    reportDir: 'cypress/results',
-    reportFilename: '[name].html',
-    overwrite: true,
-    html: true,
-    json: true,
-  },
-  e2e: {
-    setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
-      // implement node event listeners here
-    },
-  },
-});
+  pageLoadTimeout: 60000,
+})
