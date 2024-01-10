246abc17c7e3ea3e623c2839939dd8e438457f15
Lien Ngo <ana.ngo.1985@gmail.com>
2024-01-10 21:44:37 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,11 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   pageLoadTimeout: 60000,
-})
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
