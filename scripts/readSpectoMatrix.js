cb34c3cf8b9f31e83a5b02f62b0a230a5dfc111e
Lien Ngo <ana.ngo.1985@gmail.com>
2024-01-14 15:01:42 +0700

    Update readSpectoMatrix.js
    Allow receive dirPath when running the workflow

--- scripts/readSpectoMatrix.js
+++ scripts/readSpectoMatrix.js
@@ -1,30 +1,4 @@
 
-// scripts/readSpectoMatrix.js
-/*const fs = require('fs');
-
-function readSpectoMatrix(testPath) {
-  const files = fs.readdirSync(testPath);
-
-  let matrix = {
-    specs: [],
-  };
-
-  for (const file of files) {
-    //if (file.endsWith('.js') && !file.startsWith('x') && !file.startsWith('X') && !file.startsWith('Analytics')) {
-    if (file.endsWith('.js')) {
-      const name = file.replace(/\.[^/.]+$/, ''); // Remove file extension
-      matrix.specs.push({ name, spec: `${testPath}/${file}` });
-    }
-  }
-
-  return matrix;
-}
-
-//module.exports = { readSpectoMatrix };
-
-console.log(readSpectoMatrix('cypress/e2e/1-getting-started'));
-*/
-
 const fs = require('fs');
 const path = require('path');
 const getAllFiles = (dirPath, arrayOfFiles = []) => {
@@ -40,9 +14,6 @@ const getAllFiles = (dirPath, arrayOfFiles = []) => {
   return arrayOfFiles;
 };
 
-//module.exports = { getAllFiles };
-
-//console.log(getAllFiles('cypress/e2e/1-getting-started'));
-
-const specs = getAllFiles('cypress/e2e/1-getting-started');
+const dirPath = process.env.DIR_PATH || 'cypress/e2e/1-getting-started';
+const specs = getAllFiles(dirPath);
 process.stdout.write(`${JSON.stringify(specs)}\n`);
